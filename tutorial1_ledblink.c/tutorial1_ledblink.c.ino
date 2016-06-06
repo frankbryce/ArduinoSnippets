e1af5cce2a2c062fc423e6c5b03ca55cc855210a
John Carpenter <jonnyjack7@gmail.com>
2016-06-05 23:10:39 -0400

    A few tutorials and a simple python server for the arduino to pipe things over to the PC.

---
+++ tutorial1_ledblink.c/tutorial1_ledblink.c.ino
@@ -0,0 +1,23 @@
+/* Flashing LED
+ * ------------
+ *
+ * Turns on and off a light emitting diode(LED) connected to a digital  
+ * pin, in intervals of 2 seconds.
+ *
+ */
+
+int ledPin = 13;                 // LED connected to digital pin 13
+
+void setup()
+{
+  pinMode(ledPin, OUTPUT);      // sets the digital pin as output
+}
+
+void loop()
+{
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  delay(1000);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(1000);                  // waits for a second
+}
+
